9adb7a9be77730aa94fbd0f7970f3847fe7c49aa
Steven Yi <steven.yi@berkeley.edu>
2021-01-26 09:47:03 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, sub_attributes| # attributes should be keys
+    sub_attributes.each do |sub_attribute, names| # sub_attributes should be strings
+      names.each do |name|
+        if !pigeon_list[name]
+          pigeon_list[name] = {attribute => []}
+        elsif !pigeon_list[name][attribute]
+          pigeon_list[name][attribute] = []
+        end
+        pigeon_list[name][attribute].push(sub_attribute.to_s)
+      end
+    end
+  end
+  pigeon_list
 end
